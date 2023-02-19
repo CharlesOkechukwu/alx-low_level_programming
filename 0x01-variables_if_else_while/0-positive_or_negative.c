6cad79c833a09b32c9c7f09f76541c0370a881a8
CharlesOkechukwu <realcharlieok@gmail.com>
2023-02-18 17:21:37 -0800

    print statement to state if n is positive, negative or zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - checks an if else if statement and prints a text based on the result
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* check if n is equal, greater or less than 0 */
+	if (n == 0)
+	{
+		printf("%d is 0\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+A read me for 0x01-variables_if_else_while directory
+0-positive_or_negative.c checks is int variable n is zero, negative or positive and prints out text
