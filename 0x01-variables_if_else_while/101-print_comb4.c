e7296dafad34731af960fbcd7ea9bf553504d1f3
CharlesOkechukwu <realcharlieok@gmail.com>
2023-02-19 20:26:39 -0800

    print the smallest combination of 3 digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of 3 digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 7 || (a == '7' && b == '8' && c != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
