d6d6e8ee201d0652b9df9995e0a4c810f2d3436e
CharlesOkechukwu <realcharlieok@gmail.com>
2023-02-21 21:18:57 -0800

    print string _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the string _putchar using the _putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+A file with description of our header files
+0-putchar.c prints the text _putchar using a custom function _putchar
