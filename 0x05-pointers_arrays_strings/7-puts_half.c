a23ceac1d6874a1927ab2674f0406f141b53bbb2
CharlesOkechukwu <realcharlieok@gmail.com>
2023-02-28 18:17:17 -0800

    print half the line of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the characters of a string
+ * @str: string passed into the function
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count;
+	int n, even;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	if ((count % 2) == 0)
+	{
+		even = count / 2;
+		while (even < count)
+		{
+			_putchar(str[even]);
+			even++;
+		}
+	}
+	else
+	{
+		n = (count - 1) / 2;
+		while (n < count)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
