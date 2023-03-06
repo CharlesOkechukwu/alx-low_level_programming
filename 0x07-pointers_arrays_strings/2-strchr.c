1ad5f0814062902aad1b4cdb53311d36253e2c7b
CharlesOkechukwu <realcharlieok@gmail.com>
2023-03-06 14:41:38 -0800

    search string for character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - find character in a string
+ * @s: string to scan
+ * @c: character to search for
+ *
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		if (s[i] == c)
+		{
+			s += i;
+			return (s);
+		}
+	}
+	return ('\0');
+}
