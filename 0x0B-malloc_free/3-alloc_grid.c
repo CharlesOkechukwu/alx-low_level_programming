4a38ba5662a8d5878d2754244bea20368aa9da63
CharlesOkechukwu <realcharlieok@gmail.com>
2023-03-14 12:47:08 -0700

    QQQQQQQQQprint two dimensional array with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,19 +11,24 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i, j, k;
+	int i, j, k, l;
 	int **s;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	s = malloc(height * sizeof(int));
+	s = malloc(height * sizeof(int *));
 	for (i = 0; i < height; i++)
 	{
 		s[i] = malloc(width * sizeof(int));
 		if (s[i] == NULL)
 		{
+			for (l = i; l >= 0; l--)
+			{
+				free(s[l]);
+			}
+			free(s);
 			return (NULL);
 		}
 	}
