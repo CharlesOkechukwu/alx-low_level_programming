764fdff8cb2021cb5736b5016526f69ac6e799ee
CharlesOkechukwu <realcharlieok@gmail.com>
2023-03-20 17:06:40 -0700

    print name of current file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print the current file
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
