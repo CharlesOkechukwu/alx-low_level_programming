7ba13fb614f139d7bc2c535094ffa4c9519a74ee
CharlesOkechukwu <realcharlieok@gmail.com>
2023-03-22 20:37:27 -0700

    correction 2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,24 +14,27 @@ int main(int argc, char *argv[])
 {
 	int (*p)(int, int);
 	char *ag;
+	int num1, num2;
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 	ag = argv[2];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	p = get_op_func(argv[2]);
-	if (!p)
+	p = get_op_func(ag);
+	if (p == NULL || ag[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*ag == '/' && argv[3] == 0) || (*ag == '%' && argv[3] == 0))
+	if ((*ag == '/' && num2 == 0) || (*ag == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", p(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", p(num1, num2));
 	return (0);
 }
