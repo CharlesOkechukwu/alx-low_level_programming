fd0b013bd603fd4a3cccbdf931f5a27ccc91f588
CharlesOkechukwu <realcharlieok@gmail.com>
2023-03-22 23:15:49 -0700

    final bug fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,14 +10,12 @@
  * Return: int
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int (*p)(int, int);
 	char *ag;
-	int num1, num2;
+	int num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 	ag = argv[2];
 	if (argc != 4)
 	{
@@ -25,16 +23,17 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 	p = get_op_func(argv[2]);
-	if (p == NULL || ag[1] != '\0')
+	if (!p)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	num2 = atoi(argv[3]);
 	if ((*ag == '/' && num2 == 0) || (*ag == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", p(num1, num2));
+	printf("%d\n", p(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
