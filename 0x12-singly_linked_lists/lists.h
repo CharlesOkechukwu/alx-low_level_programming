49f8d0566631f758fca0512048638bfbbf645c86
CharlesOkechukwu <realcharlieokQ@gmail.com>
2023-03-30 12:40:32 -0700

    print linked lists in c programming language

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 #include <stddef.h>
 
 int _putchar(char c);
-size_t print_list(const list_t *h);
 
 /**
  * struct list_s - singly linked list
@@ -19,5 +18,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
 
 #endif
