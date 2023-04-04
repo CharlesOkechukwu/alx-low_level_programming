3781ed4181de9e3845e1ef15d622cd26d1f6b492
CharlesOkechukwu <realcharlieokQ@gmail.com>
2023-04-04 09:38:49 -0700

    delete first node in a list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,6 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int num;
 
-	num = (*head)->n;
 	if (*head == NULL)
 	{
 		return (0);
@@ -21,6 +20,7 @@ int pop_listint(listint_t **head)
 	else
 	{
 		temp = (*head)->next;
+		num = (*head)->n;
 		free(*head);
 		*head = temp;
 	}
