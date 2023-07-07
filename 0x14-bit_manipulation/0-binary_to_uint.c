67bebbce5a3b2ebeebaacd678ee1a737f0b2a3b7
CharlesOkechukwu <realcharlieok@gmail.com>
2023-07-07 06:59:59 -0700

    binary to int, set bit, clear bit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary number to int
+ * @b: pointer pointing to the binary number
+ *
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = 0, index = 0, sum = 0;
+
+	if (b == NULL)
+		return (0);
+	len = _strlen(b);
+	while (len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		if (b[len] != '0')
+			sum += 1 << index;
+		index++;
+	}
+	return (sum);
+}
+
+/**
+ * _strlen - calculate length of the string
+ * @str: the pointer to string
+ *
+ * Return: int
+ */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+# A README.md File For ALX Project On Bit Manipulation In C Programming Language
