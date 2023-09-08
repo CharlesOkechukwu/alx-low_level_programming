118e0c742430d4ebe08070a5594af60f84bac7af
CharlesOkechukwu <realcharlieok@gmail.com>
2023-09-08 15:41:06 -0700

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash table
+ * @ht: table to print
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int flag = 0;
+
+	if (ht == NULL)
+		exit(0);
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag == 1)
+				putchar(',');
+			printf("'%s' : '%s'", node->key, node->value);
+			flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
